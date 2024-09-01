605d4d12c7de8e85c9937b9e92aa74d6f4bad006
itrypolskyi <itrypolskyi@hubspot.com>
2024-09-01 13:55:26 +0200

    Add second variant of raw ore process

--- project-loom/data/recipes.lua
+++ project-loom/data/recipes.lua
@@ -8,8 +8,37 @@ data:extend({
     subgroup = "loom-ore-processing",
     results = { { name = "iron-ore", amount = 5 }, { name="copper-ore", amount = 1} },
     order = "c",
-    icon = "__project-loom-assets__/graphics/icons/raw-ores.png",
-    icon_size = 64,
-    scale = 64 / 32 * 0.35,
-}
+    icons = {
+      {
+        icon = "__project-loom-assets__/graphics/icons/raw-ores.png",
+        icon_size = 64,
+      },{
+        icon = "__base__/graphics/icons/iron-ore.png",
+        icon_size = 64,
+        scale = 0.18,
+        shift = {12, -10}
+      }
+    }
+  },
+  {
+    type = "recipe",
+    name = "loom-raw-processing-copper",
+    enabled = true,
+    energy_required = 3.5,
+    ingredients = { { "loom-raw-ore", 1 } },
+    subgroup = "loom-ore-processing",
+    results = { { name = "iron-ore", amount = 1 }, { name="copper-ore", amount = 5} },
+    order = "c",
+    icons = {
+      {
+        icon = "__project-loom-assets__/graphics/icons/raw-ores.png",
+        icon_size = 64,
+      },{
+        icon = "__base__/graphics/icons/copper-ore.png",
+        icon_size = 64,
+        scale = 0.18,
+        shift = {12, -10}
+      }
+    }
+  }
 })
